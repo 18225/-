aad731392cccb55e9dabae339ae9113c2609c1e5
18225 <61650327+18225@users.noreply.github.com>
2020-03-11 22:28:34 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,99 +1,57 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include<iostream>
 int main()
-{
-	while(1)//闰年数i，计算总天数all，智力z,一月一日至生日天数x，至测定天数y
-	{
-		int y1,y2,m1,m2,d1,d2,z,all,i,j,x,y;
-			printf("————————人体生物节律————————\n");
-			printf("请输入您的出生日期（例如出生于1988年11月13日 输入:1988,11,13）：\n");
-			scanf("%d,%d,%d,",&y1,&m1,&d1);
-			fflush(stdin);
-			printf("请您期望测定的日期（例如欲测2020年3月5日 输入:2020,3,5）：\n");
-			scanf("%d,%d,%d",&y2,&m2,&d2);
-			i=0;
-			j=y1;
-			while(j<=y2)//求闰年数
-			{
-			if(((j%4==0)&&(j%100!=0))||(j%400==0))
-				i=i+1;
-			else
-			i=i+0;
-			j=j+1;
-			}
-			if(m1==1)//开始计算一月一日至生日总天数
-				x=d1;
-			else if(m1==2)
-				x=d1+31;
-			else if(m1==3)
-				x=d1+59;
-			else if(m1==4)
-				x=d1+90;
-			else if(m1==5)
-				x=d1+120;
-			else if(m1==6)
-				x=d1+151;
-			else if(m1==7)
-				x=d1+181;
-			else if(m1==8)
-				x=d1+212;
-			else if(m1==9)
-				x=d1+243;
-			else if(m1==10)
-				x=d1+273;
-			else if(m1==11)
-				x=d1+304;
-			else
-				x=d1+334;
-			if(((y1%4==0)&&(y1%100!=0))||(y1%400==0))
-				x=x+1;//闰年加1
-			else
-				x=x+0;
-			if(m2==1)//开始计算测试总天数
-				y=d2;
-			else if(m2==2)
-				y=d2+31;
-			else if(m2==3)
-				y=d2+59;
-			else if(m2==4)
-				y=d2+90;
-			else if(m2==5)
-				y=d2+120;
-			else if(m2==6)
-				y=d2+151;
-			else if(m2==7)
-				y=d2+181;
-			else if(m2==8)
-				y=d2+212;
-			else if(m2==9)
-				y=d2+243;
-			else if(m2==10)
-				y=d2+273;
-			else if(m2==11)
-				y=d2+304;
-			else
-				y=d2+334;
-			if(((y2%4==0)&&(y2%100!=0))||(y2%400==0))
-				y=y+1;
-			else
-				y=y+0;//此处完成
-			all=(y2-y1)*365+i-x+y;
-			z=all%33;
-			if(z==0)
-				printf("您的智力节律处于周期日\n");
-			else if((z>0)&&(z<17))
-			{
-				printf("您的智力节律处于高潮期\n");
-				printf("这一天您头脑灵敏，思维敏捷，记忆力强，更具有逻辑性和解决复杂问题的能力。\n");
-			}
-			else if(z==7)
-				printf("您的智力节律处于临界日\n");
-			else
-			{
-				printf("您的智力节律处于低潮期\n");
-				printf("这一天您会出现注意力不集中，健忘，判断率降低等情况。\n");
-			
-			}
-}
-		return 0;
+{int y1,y2,y,m1,m2,d1,d2,i=0,j,x,k,sum;
+printf("请输入测定年月日(例1999.9.05):");
+scanf("%d.%d.%d",&y1,&m1,&d1);
+fflush(stdin);
+printf("请输入出生年月日(例1954.12.11):");
+scanf("%d.%d.%d",&y2,&m2,&d2);
+y=y2;
+while((y<=y1)&&(y>=y2))
+    {if((y%4==0&&y%100!=0)||(y%400==0))  
+	    i=i+1;
+	else i=i+0;y=y+1;}
+printf("闰年数为%d\n",i);
+if(m1==1) x=d1;
+else if(m1==2) x=d1+31;
+else if(m1==3) x=d1+59;
+else if(m1==4) x=d1+90;
+else if(m1==5) x=d1+120;
+else if(m1==6) x=d1+151;
+else if(m1==7) x=d1+181;
+else if(m1==8) x=d1+212;
+else if(m1==9) x=d1+243;
+else if(m1==10) x=d1+273;
+else if(m1==11) x=d1+304;
+else if(m1==12) x=d1+334;
+if((y1%4==0&&y1%100!=0)||(y1%400==0))   
+	x=x+1;
+else x=x;
+printf("1月1日至测试日有%d天\n",x);
+if(m2==1) k=d2;
+else if(m2==2) k=d2+31;
+else if(m2==3) k=d2+59;
+else if(m2==4) k=d2+90;
+else if(m2==5) k=d2+120;
+else if(m2==6) k=d2+151;
+else if(m2==7) k=d2+181;
+else if(m2==8) k=d2+212;
+else if(m2==9) k=d2+243;
+else if(m2==10) k=d2+273;
+else if(m2==11) k=d2+304;
+else if(m2==12) k=d2+334;
+if((y2%4==0&&y2%100!=0)||(y2%400==0))    //求1月1日至生日天数y
+	k=k+1;
+else k=k;
+printf("1月1日至生日有%d天\n",k);
+sum=(y1-y2)*365+i-k+x;
+printf("经历总天数为%d天\n",sum);
+j=(sum)%28;
+printf("情绪余数为%d\n",j);
+if(j==0) printf("您的情绪节律处于周期日.\n");
+else if(j<14) printf("您的情绪节律处于高潮期.\n");
+else if(j==14) printf("您的情绪节律处于临界日.\n");
+else printf("您的情绪节律处于低潮期.\n");
+system("pause");
 }
